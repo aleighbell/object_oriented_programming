3d78490a57e0cff2349e814a3736a59d54ea8fd5
aleighbell <aleighbell@gmail.com>
2016-08-28 11:57:18 -0400

    created edit method

--- crm assignment.rb
+++ crm assignment.rb
@@ -3,44 +3,82 @@
 
 class Contact
   @@contacts = []
-  @@id = 1
-  attr_accessor :first_name, :last_name, :email, :note, :id
+  attr_accessor :first_name, :last_name, :email, :note,
 
-  def initialize (first_name, last_name, email, note, id)
+  def initialize (first_name, last_name, email, note)
     @first_name = first_name
     @last_name = first_name
     @email = email
     @note = note
-    @id = @@id
-    @@id += 1
   end
-
-  def add_new_contact
-    new_contact = Contact.new(first_name, last_name, email, note, id)
-    @@contacts << new_contact
-    return new_contact
-  end
-
-  def edit_contact(first_name, last_name, email, note, id)
 
   def self.all
     return @@contacts
   end
 
-  def self.find(id)
+  def self.find(attribute)
     @@contacts.each do |contact|
-      return contact if contact.id == id
+      return contact if contact.attribute == id.attribute
+      puts "Who ya lookin for, lovely? first_name, last_name, email, note"
 
+  def add_new_contact
+    new_contact = Contact.new(first_name, last_name, email, note)
+    @@contacts << new_contact
+    return new_contact
+  end
 
+  def edit_contact
+    puts "please tell me what you would like to update. first_name, last_name, email or note"
+    answer = gets
 
-    print "First Name, please"
-    first_name = gets
+    if answer == "first_name"
+      puts "new name, please"
+      new_name = gets
+      @first_name = new_name
+      puts "thank you for the change"
+      puts "would you like updated contact"
 
-    print "Last Name, please"
-    last_name = gets
+    if answer == "Yes"
+      puts "#{@first_name}, #{@last_name}, #{@email}, #{@note}"
 
-    print "Enter Email, please"
-    email = gets
+    if answer == "No"
+      puts "okay!"
 
-    print "Enter a note, please"
-    note = gets
+    elsif answer == "last_name"
+      puts "new name, please"
+      new_name = gets
+      @last_name = new_name
+      puts "thank you for the change"
+      puts "would you like updated contact"
+
+    if answer == "Yes"
+      puts "#{@first_name}, #{@last_name}, #{@email}, #{@note}"
+
+    if answer == "No"
+      puts "okay!"
+
+    if answer == "email"
+      puts "new email, please"
+      email = gets
+      @email = new_email
+      puts "thank you for the change"
+      puts "would you like updated contact"
+
+    if answer == "Yes"
+      puts "#{@first_name}, #{@last_name}, #{@email}, #{@note}"
+
+    if answer == "No"
+      puts "okay!"
+
+    elsif answer == "note"
+      puts "what you wanna say, love?"
+      note = gets
+      @note = new_note
+      puts "thank you for the change"
+      puts "would you like updated contact"
+
+    if answer == "Yes"
+      puts "#{@first_name}, #{@last_name}, #{@email}, #{@note}"
+
+    if answer == "No"
+      puts "okay!"
